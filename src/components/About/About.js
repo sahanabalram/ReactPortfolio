a71dc0db99dcb0bde0d5b13355585db025b32481
Sahana Balram <sahana2008balram@gmail.com>
2018-04-17 14:19:22 -0700

    Include image and styling about component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,7 +8,7 @@ class AboutContainer extends Component {
             <div className="card border-primary mb-3">
                 <div className="card-header">
                    <Pulse><h3>About <i className="fa fa-user" aria-hidden="true"></i></h3></Pulse>
-                   
+                   <img className="img-responsive img-circle" src="https://media.licdn.com/dms/image/C5103AQEqYIy-Zi61fA/profile-displayphoto-shrink_200_200/0?e=1529186400&v=beta&t=vdTJ27j1ceRhq9mY0ZuMf1WUuqsgtpUU7rlYZN37uxA"  alt="Sahana Balram"/>
 
                 </div>
                 <div className="card-body">
