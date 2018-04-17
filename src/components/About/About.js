bad6e8a816d93212419c14fef2d805c7cae1c8ee
Sahana Balram <sahana2008balram@gmail.com>
2018-04-17 14:57:48 -0700

    Include deployed links

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,10 +8,11 @@ class AboutContainer extends Component {
             <div className="card border-primary mb-3">
                 <div className="card-header">
                    <Pulse><h3>About <i className="fa fa-user" aria-hidden="true"></i></h3></Pulse>
-                   <img className="img-responsive img-circle" src="https://media.licdn.com/dms/image/C5103AQEqYIy-Zi61fA/profile-displayphoto-shrink_200_200/0?e=1529186400&v=beta&t=vdTJ27j1ceRhq9mY0ZuMf1WUuqsgtpUU7rlYZN37uxA"  alt="Sahana Balram"/>
+                 
 
                 </div>
                 <div className="card-body">
+                <img className="img-responsive img-circle" src="https://media.licdn.com/dms/image/C5103AQEqYIy-Zi61fA/profile-displayphoto-shrink_200_200/0?e=1529186400&v=beta&t=vdTJ27j1ceRhq9mY0ZuMf1WUuqsgtpUU7rlYZN37uxA"  alt="Sahana Balram"/>
                     <p className="card-text text-success">{this.props.personalInfo.about}</p>
                 </div>
             </div>
