71e4d9bdb2a6da8776db5850177233de385355ea
Sahana Balram <sahana2008balram@gmail.com>
2018-04-18 16:16:15 -0700

    Bring back original code

--- src/App.js
+++ src/App.js
@@ -14,22 +14,21 @@ class App extends Component {
   render() {
     return (
       <div>
-
-        <JumbotronContainer personalInfo={ResumeDetails.personalInfo}/>
-        <Grid>
-          <Row className="main-row">
-            <Col s={12} md={4} lg={3}>
-              <Row><AboutContainer personalInfo={ResumeDetails.personalInfo}/></Row>
-              <Row><SummaryContainer summary={ResumeDetails.summary}/></Row>
-              <Row><EducationContainer education={ResumeDetails.education}/></Row>
-              <Row><SkillContainer/></Row>
-            </Col>
-            <Col s={12} md={8} lg={9}>
-              <ProjectContainer projects={ResumeDetails.projects}/>
-            </Col>
-          </Row>
-        </Grid>
-        <Footer/>
+      <JumbotronContainer personalInfo={ResumeDetails.personalInfo}/>
+      <Grid>
+      <Row className="main-row">
+        <Col s={12} md={4} lg={3}>
+          <Row><AboutContainer personalInfo={ResumeDetails.personalInfo}/></Row>
+          <Row><SummaryContainer summary={ResumeDetails.summary}/></Row>
+          <Row><EducationContainer education={ResumeDetails.education}/></Row>
+          <Row><SkillContainer/></Row>
+        </Col>
+        <Col s={12} md={8} lg={9}>
+          <ProjectContainer projects={ResumeDetails.projects}/>
+        </Col>
+      </Row>
+    </Grid>
+    <Footer/>
       </div>
     );
   }
