ea8012dcf12b7e62f642235e5e5d38604cfd505e
Sahana Balram <sahana2008balram@gmail.com>
2018-04-18 22:56:20 -0700

    Add style and change display of card

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -10,12 +10,23 @@ class EducationContainer extends Component {
             <div>
                 <div className="card border-primary mb-3">
                     <div className="card-header">
-                    <Pulse><h3 id="education">Education <i class="fa fa-graduation-cap" aria-hidden="true"></i></h3></Pulse>
+                        <Pulse>
+                            <h3 id="education">Education
+                                <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                            </h3>
+                        </Pulse>
                     </div>
                     <div className="card-body">
-                        <p className="card-text text-success">{this.state.education.map(educationLine => (<p>{educationLine.degree} {educationLine.university}<span class="badge badge-pill badge-primary">{educationLine.year}</span></p>
-                            
-                        ))}</p>
+                        <p className="card-text">{this
+                                .state
+                                .education
+                                .map(educationLine => (
+                                    <p>
+                                        <span className="edu-degree">{educationLine.degree}</span>
+                                        <br/> <span>{educationLine.university} ({educationLine.year})</span>
+                                    </p>
+
+                                ))}</p>
 
                     </div>
                 </div>
