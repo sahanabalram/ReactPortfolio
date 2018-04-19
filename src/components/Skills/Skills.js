07a83effa3b3d78db6d7393df64a035e29d84fd3
Sahana Balram <sahana2008balram@gmail.com>
2018-04-19 15:12:32 -0700

    Include font-awesome

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -14,7 +14,7 @@ class SkillsContainer extends Component {
                     </Pulse>
                 </div>
                 <div className="card-body">
-                    <h3 className="card-text skills">Web Technology</h3>
+                    <h3 className="card-text skills">Web Technology <i class="fa fa-laptop" aria-hidden="true"></i></h3>
                     <span className="badge badge-pill badge-success">HTML</span>
                     <span className="badge badge-pill badge-success">CSS</span>
                     <span className="badge badge-pill badge-success">Javascript</span>
@@ -27,26 +27,28 @@ class SkillsContainer extends Component {
                     <span className="badge badge-pill badge-success">Drupal</span>
 
                     <h3 className="card-text skills">
-                        Version Control
+                        Version Control <i class="fa fa-code-fork" aria-hidden="true"></i>
+
                     </h3>
                     <span className="badge badge-pill badge-success">Git</span>
                     <span className="badge badge-pill badge-success">Github</span>
                     <span className="badge badge-pill badge-success">Bitbucket</span>
 
                     <h3 className="card-text skills">
-                        Databases
-                    </h3>
+                        Databases <i class="fa fa-database" aria-hidden="true"></i>
+                    </h3> 
                     <span className="badge badge-pill badge-success">NoSQL: MongoDB,Firebase</span>
                     <span className="badge badge-pill badge-success">SQL: MySQL</span>
 
                     <h3 className="card-text skills">
-                        Cloud
+                        Cloud <i class="fa fa-cloud" aria-hidden="true"></i>
                     </h3>
                     <span className="badge badge-pill badge-success">Heroku</span>
                     <span className="badge badge-pill badge-success">Google Firebase</span>
 
                     <h3 className="card-text skills">
-                        Testing Tools
+                        Testing Tools <i class="fa fa-check" aria-hidden="true"></i>
+
                     </h3>
                     <span className="badge badge-pill badge-success">Mocha</span>
                     <span className="badge badge-pill badge-success">Chai</span>
