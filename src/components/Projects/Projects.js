566a95b6fb11c1945ad179b7a0603652b7ae3844
Sahana Balram <sahana2008balram@gmail.com>
2018-04-18 21:02:55 -0700

    Original card

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
 import {Grid, Row, Col} from 'react-bootstrap';
 import Pulse from 'react-reveal/Pulse';
+import Jump from 'react-reveal/Jump';
 import './Projects.css';
-
 class ProjectContainer extends Component {
+    constructor(props) {
+        super(props);
+        // Define inline styles
+        this.styles = {
+            backgroundColor: this.props.projects.color || "#6fc0d1"
+        };
+    }
     state = {
 
         projects: this.props.projects || []
@@ -19,14 +26,21 @@ class ProjectContainer extends Component {
                             .projects
                             .map(projectLine => (
                                 <Col s={12} md={4} lg={4}>
-                                    <div className="card border-primary mb-3">
-                                        <img className="card-img-top" src={projectLine.screenshot} alt={projectLine.heading}/>
-                                        <div className="card-body">
-                                            <Pulse><h4>{projectLine.heading}</h4></Pulse>
-                                            <p className="card-text">Project description: {projectLine.description}</p>
-                                            <a href={projectLine.link} className="btn btn-outline-success" target="_blank">Deployed Link</a>
+                                    <Jump>
+                                        <div className="card border-primary mb-3">
+                                            <img
+                                                className="card-img-top"
+                                                src={projectLine.screenshot}
+                                                alt={projectLine.heading}/>
+                                            <div className="card-body">
+                                                <Pulse>
+                                                    <h4>{projectLine.heading}</h4>
+                                                </Pulse>
+                                                <p className="card-text">Project description: {projectLine.description}</p>
+                                                <a href={projectLine.link} className="btn btn-outline-success" target="_blank">Deployed Link</a>
+                                            </div>
                                         </div>
-                                    </div>
+                                    </Jump>
                                 </Col>
                             ))}
 
