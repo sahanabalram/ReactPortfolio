412b31617d7046786243e46b2b7870cb3cf44de4
Sahana Balram <sahana2008balram@gmail.com>
2018-04-26 21:34:14 -0700

    Add react-bootstrap navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,38 +1,22 @@
 import React, {Component} from "react";
-// import { BrowserRouter as Router } from 'react-router-dom';
-import  "./Navbar.css";
+import {Navbar, NavItem, Nav, NavDropdown, MenuItem} from "react-bootstrap";
+
+import "./Navbar.css";
 class NavbarContainer extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                <div>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarColor02"
-                    aria-controls="navbarColor02"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                </div>
-                <div className="collapse navbar-collapse" id="navbarColor02">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item nav-right">
-                            <a className="nav-link" href="#profile-link">Profile
-                                <span className="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li className="nav-item nav-right">
-                            <a className="nav-link" href="#education-link">Edu-Skill</a>
-                        </li>
-                        <li className="nav-item nav-right">
-                            <a className="nav-link" href="#projects-link">Portfolio</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+            <Navbar className="top-navbar">
+                <Navbar.Header>
+                    <Navbar.Brand>
+                    </Navbar.Brand>
+                </Navbar.Header>
+                <Navbar.Text pullRight>
+                    <Navbar.Link href="#profile-link"> Profile </Navbar.Link>
+                    <Navbar.Link href="#education-link"> Education </Navbar.Link>
+                    <Navbar.Link href="#skills-link"> Skills </Navbar.Link>
+                    <Navbar.Link href="#projects-link"> Projects </Navbar.Link>
+                </Navbar.Text>
+            </Navbar>
         );
     }
 }
