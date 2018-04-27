a5c4d872eb306acd9ce87392dcbadaaa12936ccc
Sahana Balram <sahana2008balram@gmail.com>
2018-04-26 20:21:17 -0700

    Include Navbarcontainer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Grid, Col, Row} from 'react-bootstrap';
 import './App.css';
 import JumbotronContainer from './components/Jumbotron';
-// import NavbarContainer from './components/Navbar';
+import NavbarContainer from './components/Navbar';
 import EducationContainer from './components/Education';
 import SkillsTagCloudContainer from "./components/SkillsTagCloud";
 import ProjectContainer from './components/Projects';
@@ -14,18 +14,17 @@ class App extends Component {
   render() {
     return (
       <div>
-      {/*<NavbarContainer/>*/}
+        <NavbarContainer/>
         <JumbotronContainer personalInfo={ResumeDetails.personalInfo}/>
         <Grid>
           <Row className="about-row">
             <AboutContainer personalInfo={ResumeDetails.personalInfo}/>
-            {/*<SummaryContainer summary={ResumeDetails.summary}/>*/}
           </Row>
           <Row className="main-row">
             <Col s={12} md={12} lg={6}>
               <EducationContainer education={ResumeDetails.education}/>
-              </Col>
-              <Col s={12} md={12} lg={6}>
+            </Col>
+            <Col s={12} md={12} lg={6}>
               <SkillsTagCloudContainer skills={ResumeDetails.skills}/>
             </Col>
           </Row>
